13afcabcbca1d8223f3400cadb2f11a65007b164
Siarhei Ivashka <s.ivashka@andersenlab.com>
2023-06-06 13:36:43 +0300

    Copy a group to the next room

--- Class1.cs
+++ Class1.cs
@@ -20,22 +20,27 @@ namespace PlaceGroup
                               ref string message,
                               ElementSet elements)
         {
+            var uiapp = commandData.Application;
+            var doc = uiapp.ActiveUIDocument.Document;
+            var sel = uiapp.ActiveUIDocument.Selection;
+            var selectionFilter = new GroupPickFilter();
+
             try
             {
+                var pickedReference = sel.PickObject(ObjectType.Element, selectionFilter, "Please select a group");
+                var elem = doc.GetElement(pickedReference);
+                var group = elem as Group;
 
-                UIApplication uiapp = commandData.Application;
-                Document doc = uiapp.ActiveUIDocument.Document;
+                var origin = GetElementCenter(group);
+                var room = GetRoomOfGroup(doc, origin);
+                var sourceCenter = GetRoomCenter(room);
+                var coords = $"X: {sourceCenter.X} \r\n Y: {sourceCenter.Y} \r\n Z: {sourceCenter.Z}";
+                TaskDialog.Show("Source room Center", coords);
 
-                Selection sel = uiapp.ActiveUIDocument.Selection;
+                //var point = sel.PickPoint("Pick a point to place group");
+                var point = sourceCenter + new XYZ(20, 0, 0);
 
-                GroupPickFilter selectionFilter = new GroupPickFilter();
-                Reference pickedReference = sel.PickObject(ObjectType.Element, selectionFilter, "Please select a group");
-                Element elem = doc.GetElement(pickedReference);
-                Group group = elem as Group;
-
-                XYZ point = sel.PickPoint("Pick a point to place group");
-
-                Transaction trans = new Transaction(doc);
+                var trans = new Transaction(doc);
                 trans.Start("Lab");
                 doc.Create.PlaceGroup(point, group.GroupType);
                 trans.Commit();
@@ -52,13 +57,43 @@ namespace PlaceGroup
                 return Result.Failed;
             }
         }
+
+        public XYZ GetElementCenter(Element element)
+        {
+            var boundingBox = element.get_BoundingBox(null);
+            return (boundingBox.Max + boundingBox.Min) / 2;
+        }
+
+        Room GetRoomOfGroup(Document doc, XYZ point)
+        {
+            var collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            Room room = null;
+            foreach (var element in collector)
+            {
+                room = element as Room;
+                if (room != null && room.IsPointInRoom(point))
+                {
+                    break;
+                }
+            }
+
+            return room;
+        }
+
+        public XYZ GetRoomCenter(Room room)
+        {
+            var boundCenter = GetElementCenter(room);
+            var locationPoint = (LocationPoint) room.Location;
+            return new XYZ(boundCenter.X, boundCenter.Y, locationPoint.Point.Z);
+        }
     }
 
     public class GroupPickFilter : ISelectionFilter
     {
         public bool AllowElement(Element elem)
         {
-            return elem.Category.Id.Value.Equals((int)BuiltInCategory.OST_IOSModelGroups);
+            return elem.Category.Id.IntegerValue.Equals((int)BuiltInCategory.OST_IOSModelGroups);
         }
 
         public bool AllowReference(Reference reference, XYZ position)
